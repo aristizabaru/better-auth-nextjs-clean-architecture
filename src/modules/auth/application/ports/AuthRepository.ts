f05dc6265d3243e60fbcef133f557efbbc390b3e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2026-02-09 21:38:37 -0500

    feat: Add signInWithEmail to AuthRepository
    Expose a signInWithEmail method on the AuthRepository interface to support email sign-in flows. Adds imports for SignInEmailInput and SignInResult and defines signInWithEmail(input: SignInEmailInput): Promise<SignInResult> so implementations can return the sign-in result asynchronously.

--- src/modules/auth/application/ports/AuthRepository.ts
+++ src/modules/auth/application/ports/AuthRepository.ts
@@ -1,6 +1,9 @@
+import { SignInEmailInput } from "../dtos/SignInEmailInput";
+import { SignInResult } from "../dtos/SignInResult";
 import { SignUpEmailInput } from "../dtos/SignUpEmailInput";
 import { SignUpResult } from "../dtos/SignUpResult";
 
 export interface AuthRepository {
   signUpWithEmail(input: SignUpEmailInput): Promise<SignUpResult>;
+  signInWithEmail(input: SignInEmailInput): Promise<SignInResult>;
 }
